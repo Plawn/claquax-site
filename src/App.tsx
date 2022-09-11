d439010c143692ea62b9ca06b1f30f01e4133416
herbosa <antoine.herbosa@epitech.eu>
2022-09-11 17:56:35 +0200

    feat (App): Add logo on main page.
    aadd pakistan logo on page with projected shadow on.

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import { debiles } from './lesDebiles';
 import { Debile } from './Debile';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import Spacer from './common/Spacer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Typography variant='h1' className="HeaderTypo" >
-          Claquax Order
-        </Typography>
+      <Box
+        component="img"
+        style={{
+          WebkitFilter: "drop-shadow(0px 0px 17px rgba(0,0,0,0.6))",
+          filter: "drop-shadow(0px 0px 11px rgba(0,0,0,0.6))",
+        }}
+        sx={{
+          width: 7000,
+          maxWidth: { xs: 7000, md: 550 },
+        }}
+        src="/Claquax-Order.png"
+        alt="claquax logo"
+      />
       </header>
       <main
         style={{
