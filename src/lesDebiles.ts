d1375a062cf730f1ee63f14d56dfe434da2f9891
Plawn <plawn.yay@gmail.com>
2023-07-27 10:16:09 +0200

    Update lesDebiles.ts

--- src/lesDebiles.ts
+++ src/lesDebiles.ts
@@ -17,7 +17,7 @@ export type UnDebile = {
 export const debiles: UnDebile[] = [
 {
     id: 0,
-    name: "Camille le nul",
+    name: "Camille",
     photoUrl: "https://media.licdn.com/dms/image/D4E03AQFxbZM5gXBk2g/profile-displayphoto-shrink_800_800/0/1682060410912?e=1695859200&v=beta&t=Ez31CxtWEmjKiXTNjg7tvMVASKA3oDtJgq_-zZVW_aE",
     bio: "*Changer de voiture, c'est cool*",
     items: [],
